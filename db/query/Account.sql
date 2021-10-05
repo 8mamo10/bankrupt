02dd22b09c5ff0a9ebeaa5349435193313f114d5
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-10-06 03:32:14 +0900

    Add AddAccountBalance

--- db/query/Account.sql
+++ db/query/Account.sql
@@ -31,3 +31,9 @@ WHERE id = $1;
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1
 FOR NO KEY UPDATE;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
