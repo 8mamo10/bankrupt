b4a6c43ec09dcb50a951b0e735a58fe835900504
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-12-02 23:26:16 +0900

    Add test for LoginUserAPI, case InvalidUsername

--- api/user_test.go
+++ api/user_test.go
@@ -270,6 +270,21 @@ func TestLoginUserAPI(t *testing.T) {
 				require.Equal(t, http.StatusInternalServerError, recorder.Code)
 			},
 		},
+		{
+			name: "InvalidUsername",
+			body: gin.H{
+				"username": "invalid-user#1",
+				"password": password,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetUser(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
 	}
 
 	for i := range testCases {
